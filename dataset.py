3cb066439d94aa668992ca6d6f0c59eb14b95eae
woonhayeo <woonhahaha@gmail.com>
2020-02-14 11:34:41 +0900

    add test logic (psnr)

--- dataset.py
+++ dataset.py
@@ -17,13 +17,15 @@ class Dataset:
         # () 튜플
         # {} 딕셔너리
 
-        #train_set = self.load_h5() # 입력 3000, 정답 3000개의 텐서들
+        # train_set = self.load_h5() # 입력 3000, 정답 3000개의 텐서들
         # self.train_set = tf.data.Dataset.from_tensor_slices(train_set)
         # self.train_set = self.train_set.shuffle(1000).batch(self.flags.batch)
 
         types = (tf.float32, tf.float32)
         self.train_set = tf.data.Dataset.from_generator(self.train_generator, types) # 데이터셋을 이 함수의 아웃풋으로 만들겠다 # 함수를 인자로 받음
         self.train_set = self.train_set.shuffle(1000).batch(self.flags.batch)
+
+        self.test_set = self.make_test_set()
 
     def train_generator(self):
         enc_path = os.path.join(self.flags.data, 'train') # 그냥 concatenation해도 되는데 / 때문에 경로 문제 발생 가능
@@ -40,6 +42,22 @@ class Dataset:
 
             yield from self.make_patch(enc_y, org_y, h, w) # 상위 함수가 끝나야 끝남 # make_patch가 yield로 반환했기때문에 여기도 yield를 써야함
 
+    def make_test_set(self):
+        test_path = os.path.join(self.flags.data, 'test')
+        yuv_list = glob.glob(os.path.join(test_path, "*.yuv"))
+        result = np.array([])
+        count = 0
+        for fpath in yuv_list:
+            h, w = self.yuv_meta(fpath)
+            test_y, _, _ = self.get_yuv(fpath, h, w)
+            if count == 0:
+                result = test_y
+            else:
+                result = np.vstack((result, test_y))
+            count += 1
+        result = result.reshape((12, 712, 1072))
+        return result
+
     def make_patch(self, enc, org, h, w):
         for i in range(0, h - self.flags.psize + 1, self.flags.psize):
             for j in range(0, w - self.flags.psize + 1, self.flags.psize):
@@ -52,50 +70,7 @@ class Dataset:
 
                 yield input_, label_ # 이 루프는 계속 실행이 됨. 반환 하고도 끝내면 안될 때 yield를 씀
 
-    def load_h5(self):
-        if not os.path.exists('./train.h5'):
-            self.create_h5()
-        with h5py.File('./train.h5', 'r') as hf:
-            # shape = [psize, psize] -> [psize, psize, 1]
-            input_ = np.asarray(hf.get('input_'))
-            label_ = np.asarray(hf.get('label_'))
 
-            return np.expand_dims(input_, axis=-1), np.expand_dims(label_, axis=-1)
-
-    def create_h5(self):
-        yuv_list = glob.glob(os.path.join(self.flags.data, "*.yuv"))
-        file = yuv_list[0]
-        # name_가로x세로_framerate.yuv
-        # 정규식을 다루는 패키지 = import re # 추후에 공부해보기
-        h, w = self.yuv_meta(file)
-        y = self.get_yuv(file, h, w)
-        # 이미지의 해상도 줄임
-        small_y = cv2.resize(y, None, fx=1 / 3, fy=1 / 3)
-        cubic_y = cv2.resize(small_y, (w, h), interpolation=cv2.INTER_CUBIC)
-        # normalize 0~1
-        ny = y / 255.
-        ncubic_y = cubic_y / 255.
-        # TODO: 테스트 코드
-        # cv2.imwrite('cubic.jpg', cubic_y)
-        input_list = []
-        label_list = []
-        # range(start, end, step)
-        for i in range(0, h - self.flags.psize + 1, self.flags.psize):
-            for j in range(0, w - self.flags.psize + 1, self.flags.psize):
-                # slicing [from:to, from:to]
-                input_ = ncubic_y[i:i + self.flags.psize, j:j + self.flags.psize]  # 쪼개져 나온 패치
-                label_ = ny[i:i + self.flags.psize, j:j + self.flags.psize]
-
-                input_list.append(input_)
-                label_list.append(label_)
-        input_ = np.asarray(input_list)
-        label_ = np.asarray(label_list)
-        """ hf = h5py.File('./train.h5', 'w')
-                이렇게 하면 프로그램 종료시에 hf가 없어지지만 with구문을 이용하면 해당 부분에서만 사용됨
-                """
-        with h5py.File('./train.h5', 'w') as hf:
-            hf.create_dataset('input_', data=input_)
-            hf.create_dataset('label_', data=label_)
 
     def get_yuv(self, file, h, w):
         fp = open(file, 'rb')
@@ -117,6 +92,52 @@ class Dataset:
         w = int(wh[0])
         h = int(wh[1])
         return h, w
+    
+    # 다른 방법
+    # def load_h5(self):
+    #     if not os.path.exists('./train.h5'):
+    #         self.create_h5()
+    #     with h5py.File('./train.h5', 'r') as hf:
+    #         # shape = [psize, psize] -> [psize, psize, 1]
+    #         input_ = np.asarray(hf.get('input_'))
+    #         label_ = np.asarray(hf.get('label_'))
+    # 
+    #         return np.expand_dims(input_, axis=-1), np.expand_dims(label_, axis=-1)
+    # 
+    # def create_h5(self):
+    #     yuv_list = glob.glob(os.path.join(self.flags.data, "*.yuv"))
+    #     file = yuv_list[0]
+    #     # name_가로x세로_framerate.yuv
+    #     # 정규식을 다루는 패키지 = import re # 추후에 공부해보기
+    #     h, w = self.yuv_meta(file)
+    #     y = self.get_yuv(file, h, w)
+    #     # 이미지의 해상도 줄임
+    #     small_y = cv2.resize(y, None, fx=1 / 3, fy=1 / 3)
+    #     cubic_y = cv2.resize(small_y, (w, h), interpolation=cv2.INTER_CUBIC)
+    #     # normalize 0~1
+    #     ny = y / 255.
+    #     ncubic_y = cubic_y / 255.
+    #     # TODO: 테스트 코드
+    #     # cv2.imwrite('cubic.jpg', cubic_y)
+    #     input_list = []
+    #     label_list = []
+    #     # range(start, end, step)
+    #     for i in range(0, h - self.flags.psize + 1, self.flags.psize):
+    #         for j in range(0, w - self.flags.psize + 1, self.flags.psize):
+    #             # slicing [from:to, from:to]
+    #             input_ = ncubic_y[i:i + self.flags.psize, j:j + self.flags.psize]  # 쪼개져 나온 패치
+    #             label_ = ny[i:i + self.flags.psize, j:j + self.flags.psize]
+    # 
+    #             input_list.append(input_)
+    #             label_list.append(label_)
+    #     input_ = np.asarray(input_list)
+    #     label_ = np.asarray(label_list)
+    #     """ hf = h5py.File('./train.h5', 'w')
+    #             이렇게 하면 프로그램 종료시에 hf가 없어지지만 with구문을 이용하면 해당 부분에서만 사용됨
+    #             """
+    #     with h5py.File('./train.h5', 'w') as hf:
+    #         hf.create_dataset('input_', data=input_)
+    #         hf.create_dataset('label_', data=label_)
 
 
 
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # image enhancement
 import argparse
 import time
-from skimage.metrics import peak_signal_noise_ratio as psnr
 from dataset import *
-
 
 @tf.function # GPU로 돌리라는 속성 정의
 def train_step(model, loss_fn, opt, batch):
@@ -18,13 +16,28 @@ def train_step(model, loss_fn, opt, batch):
     opt.apply_gradients(zip(grads, model.trainable_variables)) # 모든 optimizer가 가지고 있는 함수 # zip은 따로 공부하셈
     return loss, prediction #, residual
 
+@tf.function
+def test_step(model, image):
+    tf.shape(image)
+    input = tf.reshape(image, [1, 712, 1072, 1])
+    target = input
+
+    prediction = model(input)
+    clipped_prediction = tf.clip_by_value(prediction, 0., 1.)
+    prediction = tf.image.convert_image_dtype(clipped_prediction, tf.float32)
+    target = tf.image.convert_image_dtype(target, tf.float32)
+
+    PSNR = tf.image.psnr(target, prediction, max_val=1.0)
+
+    return PSNR, prediction
+
 def main(flags):
     # 데이터셋 클래스
     ds = Dataset(flags)
 
     # 모델 구성
     mod = __import__('nets', fromlist=['*'])
-    model = getattr(mod, flags.model) # SRCNN
+    model = getattr(mod, flags.model)
     model = model()
 
     # back-prop = Adam
@@ -39,14 +52,20 @@ def main(flags):
 
     train_loss = tf.metrics.Mean()
 
-
-    dir_name = flags.model + time.strftime('-%Y%m%d-%H%M%S', time.localtime(time.time())) # string float time
-    log_train = './log/' + dir_name + '/train/'
-    train_summary_writer = tf.summary.create_file_writer(log_train)
+    dir_name = flags.model + time.strftime('-%Y%m%d-%H%M%S', time.localtime(time.time())) #  string float time
+    train_log_dir = './log/' + dir_name + '/train/'
+    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
     tf.summary.trace_on() # 그래프 저장
+
+    # checkpoint
+    checkpoint_dir = './checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(model=model)
 
     count = 0
     for epoch in range(flags.epoch):
+        print("epoch:", epoch)
+
         for step, batch in enumerate(ds.train_set):
             count += 1
 
@@ -63,15 +82,40 @@ def main(flags):
 
             if count % 100 == 0: # 100번마다 한 번 씩
                 clipped = tf.clip_by_value(prediction, 0., 1.) # prediction은 0과1사이가 아닐수있음. 0보다 작은값은0, 1보다큰값은1로 만듬(rescaling이 아님)
+
+                img_num = 0
+
+                all_test_pred = []
+
+                for image in ds.test_set:
+                    PSNR, test_prediction = test_step(model, image)
+                    if img_num == 0:
+                        all_test_pred = test_prediction
+                    else:
+                        all_test_pred = np.vstack((all_test_pred, test_prediction))
+                    img_num += 1
+                    print(img_num, PSNR)
+
+                    with train_summary_writer.as_default():
+                        tf.summary.scalar('psnr'+str(img_num), float(PSNR), step=count)
+                        train_summary_writer.flush()
+                all_test_pred = np.reshape(all_test_pred, [12, 712, 1072, 1])
+
+
+
                 with train_summary_writer.as_default():
+                    #tf.summary.add_scalar('psnr', float(PSNR), step=count)
+                    tf.summary.image('test_prediction', all_test_pred, step=count)
+                    tf.summary.image('test_origin', tf.reshape(ds.test_set, [12, 712, 1072, 1]), step=count)
+
                     tf.summary.scalar('loss', train_loss.result(), step=count)
                     tf.summary.image('input', batch[0], step=count)
                     tf.summary.image('label', batch[1], step=count)
                     tf.summary.image('prediction', clipped, step=count)
-                    # tf.summary.image('residual', residual, step=count)
 
                     train_summary_writer.flush()
 
+        checkpoint.save(file_prefix=checkpoint_prefix)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -82,13 +126,13 @@ if __name__ == "__main__":
                         help="신경망 모델")
     parser.add_argument('--data', type=str, default="/home/ivpl-d14/Dataset/",
                         help="YUV 파일이 저장된 위치")
-    parser.add_argument('--psize', type=int, default=45,
+    parser.add_argument('--psize', type=int, default=45, #45
                         help="이미지 패치 사이즈")
     parser.add_argument('--batch', type=int, default=128,
                         help="배치 사이즈")
     parser.add_argument('--channel', type=int, default=1,
                         help="이미지 컬러 채널")
-    parser.add_argument('--epoch', type=int, default=3000,
+    parser.add_argument('--epoch', type=int, default=1000,
                         help="학습 반복 횟수")
 
     flags, _ = parser.parse_known_args()
